1fc24d944b77ca8d4b0623f5f7fb10f86b11af3d
dgknkrcgr <dogukankaraciger@gmail.com>
2019-10-16 22:58:59 +0300

    Update networkwebportfinder.py

--- networkwebportfinder.py
+++ networkwebportfinder.py
@@ -12,15 +12,15 @@ res=nm.scan(hosts=subnet, arguments='-sS -p ' + ports)
 
 for i in res["scan"]:
 	if res["scan"][i]["tcp"][80]["state"] == "open" and res["scan"][i]["tcp"][443]["state"] == "false":
-		f.write ("<a href=\"http://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":80</a>")
+		f.write ("<a target=\"_blank\" href=\"http://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":80</a>")
 		f.write("<br><br>")
 	if res["scan"][i]["tcp"][80]["state"] == "false" and res["scan"][i]["tcp"][443]["state"] == "open":
-		f.write ("<a href=\"https://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":443</a>")
+		f.write ("<a target=\"_blank\" href=\"https://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":443</a>")
 		f.write("<br><br>")
 	if res["scan"][i]["tcp"][443]["state"] == "open" and res["scan"][i]["tcp"][443]["state"] == "open":
-		f.write ("<a href=\"http://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":80</a>")
+		f.write ("<a target=\"_blank\" href=\"http://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":80</a>")
 		f.write("<br>")
-		f.write ("<a href=\"https://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":443</a>")
+		f.write ("<a target=\"_blank\" href=\"https://"+res["scan"][i]['addresses']['ipv4']+"\">"+res["scan"][i]['addresses']['ipv4']+":443</a>")
 		f.write("<br><br>")
 	else:
 		pass
