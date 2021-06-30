94f299cd5385b0bc54d205a467a3798d9df53a3a
d0gukank <dogukankaraciger@gmail.com>
2021-06-30 17:03:00 +0300

    Update fridanativehook.js

--- fridanativehook.js
+++ fridanativehook.js
@@ -1,9 +1,9 @@
- Module.enumerateExports("libnativeencryption-utils.so", {                
+ Module.enumerateExports("xx-utils.so", {                
       onMatch: function(e) {                            
     if(e.type == 'function') {
 		console.log("name of function = " + e.name);
 
-    if(e.name == "Java_com_airtel_agilelabs_utils_NativeEncryptionUtils_encryptData") {
+    if(e.name == "Java_xxx_NativeEncryptionUtils_encryptData") {
 		console.log("Function Decrypt recognized by name");
 		Interceptor.attach(e.address, {       
 		onEnter: function(args) {         
